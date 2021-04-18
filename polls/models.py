c38e984691947e39429ddfc89dce2978f6211cd8
Joshua <limshiawjong@gmail.com>
2021-04-18 12:08:18 +0800

    added TestMe model

--- polls/models.py
+++ polls/models.py
@@ -22,3 +22,9 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+
+class TestMe(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
